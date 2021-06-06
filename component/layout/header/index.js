06ab3d69eb1159f386ecf59c8ad2eca76cda333c
Steele Scott <60533996+steele123@users.noreply.github.com>
2021-06-06 12:28:40 -0400

    Made all the navbar links clickable with next/link

--- component/layout/header/index.js
+++ component/layout/header/index.js
@@ -1,15 +1,43 @@
+import Link from 'next/link'
+
 export default function Header() {
     return (
       <div className="bg-blue-500">
         <div className="text-gray-300 py-4 text-sm">
           <div className="flex space-x-6 items-center">
-            <img className="block pl-4" src="/images/opgg-logo.svg" height="16" alt="OP.GG" width="65" />
-            <a className="text-white border-white border-b-2">Stay Home</a>
-            <a>Champions</a>
-            <a>Stats</a>
-            <a>Leaderboards</a>
-            <a>Pro Matches</a>
-            <a>Multi-Search</a>
+            <Link href="/">
+              <img className="block pl-4 cursor-pointer" src="/images/opgg-logo.svg" height="16" alt="OP.GG" width="65" />
+            </Link>
+            <Link href="/">
+              <a className="text-white border-white border-b-2">
+                Stay Home
+              </a>
+            </Link>
+            <Link href="/champion">
+              <a>
+                Champions
+              </a>
+            </Link>
+            <Link href="/stats">
+              <a>
+                Stats
+              </a>
+            </Link>
+            <Link href="/leaderboards">
+              <a>
+                Leaderboards
+              </a>
+            </Link>
+            <Link href="/pro-matches">
+              <a>
+                Pro Matches
+              </a>
+            </Link>
+            <Link href="/multi-search">
+              <a>
+                Multi-Search
+              </a>
+            </Link>
           </div>
         </div>
       </div>
